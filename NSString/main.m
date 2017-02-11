6acd9d6661a19c42b7f86e2620f0926cfa22444a
praveen <praveen@gmail.com>
2017-02-11 13:57:43 -0500

    Initial Commit

---
+++ NSString/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  NSString
+//
+//  Created by Praveen on 2017-02-11.
+//  Copyright © 2017 Praveen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
